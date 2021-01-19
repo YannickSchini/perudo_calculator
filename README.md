34a3b2c12935a8df678bfa14ab7bf8e758693740
Yannick Schini <yannick.schini@gmail.com>
2021-01-19 21:19:46 +0100

    feat: First fixed graph
    I have a graph showing probability of getting any amount of matches with
    10 dices. The dice is supposed to be balanced and the special rule
       regarding the pacos is not taken into account yet.

--- README.md
+++ README.md
@@ -5,16 +5,19 @@ My aim here is to use something like [Dash](https://dash.plotly.com/) to have a 
 
 ## TODO List
 
-* Perform a calculation for 1 player with 1 normal dice.
 * Add the Perudo rule for the value 1.
-* Add the ability to have several dices (still only one player).
 * Add the ability to have several players.
 
 ## What I learned
 
+* Apparently Plotly requires pandas ?
+* Unittests are not run if they don't start with "test_"
+
 ## Done
 
 * Create a first static page
+* Perform a calculation for 1 player with 1 normal dice.
+* Add the ability to have several dices (still only one player).
 
 
 Yannick Schini, 2021
--- app.py
+++ app.py
@@ -1,14 +1,24 @@
 import dash
 import dash_html_components as html
+import pandas as pd
+import plotly.express as px
+import dash_core_components as dcc
+from perudo_stats import get_proba_of_given_face
 
 app = dash.Dash(__name__)
+
+df = pd.DataFrame({"Matches": range(1, 10)})
+df["proba"] = df["Matches"].apply(lambda x: get_proba_of_given_face(10, x))
+
+fig = px.line(df, x="Matches", y="proba")
 
 app.layout = html.Div(children=[
     html.H1(children="Perudo Calculator"),
     html.Div(children="""
         This is a Perudo calculator: it's aim is to help you take the best \
         decisions and win at a game of Perudo !
-    """)
+    """),
+    dcc.Graph(id='example-graph', figure=fig)
 ])
 
 if __name__ == "__main__":
