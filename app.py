c8bd3e502124502472f8fad15e5aa589c0478893
Yannick Schini <yannick.schini@gmail.com>
2021-01-19 21:53:21 +0100

    feat: Addition of a slider to chose the number of dice
    Slider added, but it has not yet been "wired" to the rest of the code !

--- app.py
+++ app.py
@@ -11,6 +11,8 @@ df = pd.DataFrame({"Matches": range(1, 10)})
 df["proba"] = df["Matches"].apply(lambda x: get_proba_of_given_face(10, x))
 
 fig = px.line(df, x="Matches", y="proba")
+dict_values = [str(x) for x in range(1, 26)]
+dict_keys = range(1, 26)
 
 app.layout = html.Div(children=[
     html.H1(children="Perudo Calculator"),
@@ -18,6 +20,13 @@ app.layout = html.Div(children=[
         This is a Perudo calculator: it's aim is to help you take the best \
         decisions and win at a game of Perudo !
     """),
+    dcc.Slider(id='Total Amount of Dices',
+               min=1,
+               max=25,
+               value=10,
+               step=None,
+               marks=dict(zip(dict_keys, dict_values)),
+               included=False),
     dcc.Graph(id='example-graph', figure=fig)
 ])
 
