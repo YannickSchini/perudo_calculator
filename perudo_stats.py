50f84df331a9f2810c7fa1b580795c6c237fc0ad
Yannick Schini <yannick.schini@gmail.com>
2021-01-24 21:49:41 +0100

    refacto(stats): More logical way to express the stats function
    After talking about it with a colleague, I've decided to change the way
    the function is formulated. This way makes more sense to me:
    proba = proba of getting k matches * proba of getting n-k non-matches *
    the binomial coefficient.
    This will also make it easier to use the same function to calculate the
    probability of getting matches with the Paco rule later on.

--- perudo_stats.py
+++ perudo_stats.py
@@ -3,12 +3,13 @@
 
 import math
 
+PROBA_OF_PACO = 1 / 6
+
 
 def get_proba_of_given_face(total_num_of_dice: int,
                             num_of_matches: int) -> float:
     """ Function to get the probability of getting the given amount of matches
     of any single face in function of the number of dice.
-    Function gotten from http://villemin.gerard.free.fr/Denombre/JeuxDes.htm
     This assumes a standard dice and does not take into account the fact that
     the Paco face in the game of Perudo can count for any other face.
 
@@ -18,9 +19,7 @@ def get_proba_of_given_face(total_num_of_dice: int,
 
     Returns:
         - the probability of getting this number of matches """
-    total_number_of_combinations = 6**total_num_of_dice
-    number_of_matching_combinations = float(
-        5**(total_num_of_dice - num_of_matches) *
-        math.comb(total_num_of_dice, num_of_matches))
-    return float(number_of_matching_combinations /
-                 total_number_of_combinations)
+    result = PROBA_OF_PACO**num_of_matches * (1 - PROBA_OF_PACO)**(
+        total_num_of_dice - num_of_matches) * math.comb(
+            total_num_of_dice, num_of_matches)
+    return result
