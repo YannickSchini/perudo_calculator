2d192759e96920e463cd6ef32c6e10b7453eb013
Yannick Schini <yannick.schini@gmail.com>
2021-01-24 22:08:47 +0100

    feat(stats): Calculation of proba for paco and normal dice faces
    I've added the calculation of the probability of matches for normal
    faces, taking into account the special value of the Paco that acts as a
    wildcard.

--- perudo_stats.py
+++ perudo_stats.py
@@ -4,10 +4,12 @@
 import math
 
 PROBA_OF_PACO = 1 / 6
+PROBA_OF_NON_PACO_FACE = 1 / 3
+# 1/3 = 1/6 (proba of getting a given face) + 1/6 (proba of getting a Paco)
 
 
-def get_proba_of_given_face(total_num_of_dice: int,
-                            num_of_matches: int) -> float:
+def get_proba_of_given_face(total_num_of_dice: int, num_of_matches: int,
+                            paco: bool) -> float:
     """ Function to get the probability of getting the given amount of matches
     of any single face in function of the number of dice.
     This assumes a standard dice and does not take into account the fact that
@@ -16,10 +18,16 @@ def get_proba_of_given_face(total_num_of_dice: int,
     Param:
         - total_num_of_dice: integer number of dice used
         - num_of_matches: integer number of faces matching
+        - paco: boolean stating if we care about the Paco face or any other
+        face of the Dices
 
     Returns:
         - the probability of getting this number of matches """
-    result = PROBA_OF_PACO**num_of_matches * (1 - PROBA_OF_PACO)**(
+    if paco:
+        match_proba = PROBA_OF_PACO
+    else:
+        match_proba = PROBA_OF_NON_PACO_FACE
+    result = match_proba**num_of_matches * (1 - match_proba)**(
         total_num_of_dice - num_of_matches) * math.comb(
             total_num_of_dice, num_of_matches)
     return result
--- test_perudo_stats.py
+++ test_perudo_stats.py
@@ -1,10 +1,15 @@
+""" Test module for the statistics calculations """
+
 import unittest
 import perudo_stats
 
 
 class TestStatistics(unittest.TestCase):
     """ Test class for the computation done in the statistics module """
-    def test_get_proba_of_given_face_with_first_values(self) -> None:
+    def test_get_proba_of_paco_face_with_first_values(self) -> None:
+        """ This function tests the probability calculator with a few values
+        gotten by hand, which explains why I did not go to bigger amount of
+        dices (or matches)"""
         # Given
         one_dice = 1
         two_dices = 2
@@ -13,18 +18,51 @@ class TestStatistics(unittest.TestCase):
         two_match = 2
         # When
         result_one_dice = perudo_stats.get_proba_of_given_face(
-            one_dice, one_match)
+            one_dice, one_match, True)
         result_two_dice = perudo_stats.get_proba_of_given_face(
-            two_dices, one_match)
+            two_dices, one_match, True)
         result_three_dice_one_match = perudo_stats.get_proba_of_given_face(
-            three_dices, one_match)
+            three_dices, one_match, True)
         result_three_dice_two_matchs = perudo_stats.get_proba_of_given_face(
-            three_dices, two_match)
+            three_dices, two_match, True)
         # Then
         self.assertAlmostEqual(result_one_dice, 1 / 6)
         self.assertAlmostEqual(result_two_dice, 10 / 36)
         self.assertAlmostEqual(result_three_dice_one_match, 75 / 6**3)
         self.assertAlmostEqual(result_three_dice_two_matchs, 15 / 6**3)
+
+    def test_get_proba_of_normal_face_with_first_values(self) -> None:
+        """ This function tests the probability calculator with a few values
+        gotten by hand, which explains why I did not go to bigger amount of
+        dices (or matches)"""
+        # Given
+        one_dice = 1
+        two_dices = 2
+        three_dices = 3
+        zero_match = 0
+        one_match = 1
+        two_match = 2
+        three_match = 3
+        # When
+        result_one_dice_no_match = perudo_stats.get_proba_of_given_face(
+            one_dice, zero_match, False)
+        result_one_dice_one_match = perudo_stats.get_proba_of_given_face(
+            one_dice, one_match, False)
+        result_two_dice_zero_match = perudo_stats.get_proba_of_given_face(
+            two_dices, zero_match, False)
+        result_two_dice_one_match = perudo_stats.get_proba_of_given_face(
+            two_dices, one_match, False)
+        result_two_dice_two_match = perudo_stats.get_proba_of_given_face(
+            two_dices, two_match, False)
+        result_three_dice_three_matchs = perudo_stats.get_proba_of_given_face(
+            three_dices, three_match, False)
+        # Then
+        self.assertAlmostEqual(result_one_dice_no_match, 2 / 3)
+        self.assertAlmostEqual(result_one_dice_one_match, 1 / 3)
+        self.assertAlmostEqual(result_two_dice_zero_match, 16 / 36)
+        self.assertAlmostEqual(result_two_dice_one_match, 16 / 36)
+        self.assertAlmostEqual(result_two_dice_two_match, 4 / 36)
+        self.assertAlmostEqual(result_three_dice_three_matchs, 8 / 6**3)
 
 
 if __name__ == "__main__":
