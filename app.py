521cee01e25e05042f3b8fc546d7ce3c55ad1b56
Yannick Schini <yannick.schini@gmail.com>
2021-01-27 18:46:17 +0100

    refacto(app): Grouped elements into specific Divs
    This refacto is done prior to adding some CSS.
    I've grouped my different elements into two groups, two Divs. This
    grouping is inspired by
    https://dash-gallery.plotly.host/dash-clinical-analytics/.

--- app.py
+++ app.py
@@ -6,21 +6,29 @@ from perudo_calculator import calculate_proba_graph_for_paco_faces
 dict_values = [str(x) for x in range(1, 26)]
 dict_keys = range(1, 26)
 
-app.layout = html.Div(children=[
-    html.H1(children="Perudo Calculator"),
-    html.Div(children="""
-        This is a Perudo calculator: it's aim is to help you take the best \
-        decisions and win at a game of Perudo !
-    """),
-    dcc.Slider(id="Total Amount of Dices",
-               min=1,
-               max=25,
-               value=10,
-               step=None,
-               marks=dict(zip(dict_keys, dict_values)),
-               included=False),
-    dcc.Graph(id="Proba of matches")
-])
+app.layout = html.Div(
+    id="app-container",
+    children=[
+        # Left column
+        html.Div(id="left-column",
+                 children=[
+                     html.H1(children="Perudo Calculator"),
+                     html.Div(children="""
+                        This is a Perudo calculator: it's aim is to help you \
+                        take the best decisions and win at a game of Perudo !
+                        """),
+                     dcc.Slider(id="Total Amount of Dices",
+                                min=1,
+                                max=25,
+                                value=10,
+                                step=None,
+                                marks=dict(zip(dict_keys, dict_values)),
+                                included=False)
+                 ]),
+        # Right column
+        html.Div(id="right-column",
+                 children=[dcc.Graph(id="Proba of matches")])
+    ])
 
 if __name__ == "__main__":
     app.run_server(debug=True)
