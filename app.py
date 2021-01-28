d6f5f37425148b289717fcf44c1806a31367d3c8
Yannick Schini <yannick.schini@gmail.com>
2021-01-28 22:09:20 +0100

    refacto(UI): Grouping of description elements
    Following the example of
    https://dash-gallery.plotly.host/dash-clinical-analytics/, I've grouped
    elements that describe the page into a dedicated function.
    I've also moved around things a bit in the CSS, to center the title.

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import dash_html_components as html
 import dash_core_components as dcc
 from server import app
 from perudo_calculator import calculate_proba_graph_for_paco_faces
+from perudo_calculator import description_card
 
 dict_values = [str(x) for x in range(1, 26)]
 dict_keys = range(1, 26)
@@ -12,11 +13,7 @@ app.layout = html.Div(
         # Left column
         html.Div(id="left-column",
                  children=html.Div(children=[
-                     html.H1(children="Perudo Calculator"),
-                     html.Div(children="""
-                        This is a Perudo calculator: it's aim is to help you \
-                        take the best decisions and win at a game of Perudo !
-                        """),
+                     description_card(),
                      dcc.Slider(id="Total Amount of Dices",
                                 min=1,
                                 max=25,
--- perudo_calculator.py
+++ perudo_calculator.py
@@ -1,6 +1,7 @@
 import dash
 import pandas as pd
 import plotly.express as px
+import dash_html_components as html
 from plotly.graph_objects import Figure as Figure
 from perudo_stats import get_proba_of_given_face
 from server import app
@@ -21,3 +22,22 @@ def calculate_proba_graph_for_paco_faces(number_of_dice: int) -> Figure:
                   y=["Paco faces matches", "Normal faces matches"],
                   title="Probability of getting x matches")
     return fig
+
+
+def description_card() -> html.Div:
+    """ Generate the description card of the page.
+    returns a Div containing the title & descriptions """
+    return html.Div(
+        id="description-card",
+        children=[
+            html.H1("Perudo Calculator"),
+            html.H3("Welcome to our Perudo helper page !"),
+            html.Div(
+                id="intro",
+                children="Use statistics to help you make the best decisions \
+                and win against your best friends and enemies at the \
+                game of Perudo ! Input data about your game below, and\
+                use the statistics to the right to win !",
+            ),
+        ],
+    )
