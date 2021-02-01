c9b2eed4377bc96f0eae8a5032583b62b34af7d0
Yannick Schini <yannick.schini@gmail.com>
2021-02-01 19:16:04 +0100

    feat(stats): Calculation to get posterior proba
    Implementation of the calculation to get posterior proba.
    I've done some hand calculation to simplify the formula and not call
    get_proba_of_given_face().

--- perudo_stats.py
+++ perudo_stats.py
@@ -8,7 +8,7 @@ PROBA_OF_NON_PACO_FACE = 1 / 3
 # 1/3 = 1/6 (proba of getting a given face) + 1/6 (proba of getting a Paco)
 
 
-def get_proba_of_given_face(total_num_of_dice: int, num_of_matches: int,
+def get_proba_of_given_face(total_dice_num: int, total_match_num: int,
                             paco: bool) -> float:
     """ Function to get the probability of getting the given amount of matches
     of any single face in function of the number of dice.
@@ -16,8 +16,8 @@ def get_proba_of_given_face(total_num_of_dice: int, num_of_matches: int,
     the Paco face in the game of Perudo can count for any other face.
 
     Param:
-        - total_num_of_dice: integer number of dice used
-        - num_of_matches: integer number of faces matching
+        - total_dice_num: integer number of dice used
+        - total_match_num: integer number of faces matching
         - paco: boolean stating if we care about the Paco face or any other
         face of the Dices
 
@@ -27,13 +27,17 @@ def get_proba_of_given_face(total_num_of_dice: int, num_of_matches: int,
         match_proba = PROBA_OF_PACO
     else:
         match_proba = PROBA_OF_NON_PACO_FACE
-    result = match_proba**num_of_matches * (1 - match_proba)**(
-        total_num_of_dice - num_of_matches) * math.comb(
-            total_num_of_dice, num_of_matches)
+
+    result = match_proba**total_match_num * (
+        1 - match_proba)**(total_dice_num - total_match_num) * math.comb(
+            total_dice_num, total_match_num)
+
     return result
 
 
-def get_posterior_proba() -> float:
+def get_posterior_proba(total_dice_num: int, total_match_num: int,
+                        own_dice_num: int, own_match_num: int,
+                        paco: bool) -> float:
     """ Function to get the posterior probability, this means the probability of
     getting X matches in total in all the dices knowing you have Y matches in
     your Z dices.
@@ -62,10 +66,24 @@ def get_posterior_proba() -> float:
                     math.comb(W, X)
 
     Parameters:
-        - Total amount of dices (W)
-        - Total amount of matches (X)
-        - One's amount of dices (Z)
-        - One's amount of matches (Y)
+        - total_dice_num: total amount of dices (W)
+        - total_match_num: total amount of matches (X)
+        - own_dice_num: one's amount of matches (Y)
+        - own_match_num: one's amount of dices (Z)
+        - paco: if we consider Paco or non Paco faces (bool)
 
     """
-    raise NotImplementedError
+    if paco:
+        match_proba = PROBA_OF_PACO
+    else:
+        match_proba = PROBA_OF_NON_PACO_FACE
+
+    other_dice_num = total_dice_num - own_dice_num
+    other_match_num = total_match_num - own_match_num
+    other_non_match_num = other_dice_num - other_match_num
+
+    result = match_proba**other_dice_num * (
+        1 - match_proba)**other_non_match_num * math.comb(
+            other_dice_num, other_non_match_num)
+
+    return result
